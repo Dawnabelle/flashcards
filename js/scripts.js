b4ceb129c85190712f4bf9cc4609651ff6933117
Brenda Franco and Dawn Mott <dawn@>
2018-03-20 13:49:08 -0700

    add clickable flashcards to html, upate css and js files

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#javascript-showing").toggle();
+    $("#javascript-hidden").toggle();
+  });
+  $(".clickable2").click(function() {
+    $("#operators-showing").toggle();
+    $("#operators-hidden").toggle();
+  });
+  $(".clickable3").click(function() {
+    $("#variables-showing").toggle();
+    $("#variables-hidden").toggle();
+  });
+});
